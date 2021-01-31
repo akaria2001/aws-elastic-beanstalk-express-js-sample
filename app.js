c153241ac0eea685e49d64a4234ba758b62d1578
root <root@rh7pc.local>
2021-01-31 11:15:17 +0000

    Manual Review 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to IT HowTo, my name is Amit and I am learning AWS DevOps! Full pipeline test'));
+app.get('/', (req, res) => res.send('Welcome to IT HowTo, my name is Amit and I am learning AWS DevOps! Full pipeline test manual review'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
