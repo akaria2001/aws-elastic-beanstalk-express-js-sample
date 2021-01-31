0b011d880518a429b9447671832cbe4dcb5bf831
root <root@rh7pc.local>
2021-01-31 10:53:29 +0000

    Welcome to IT HowTo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! from Amit! A'));
+app.get('/', (req, res) => res.send('Welcome to IT HowTo, my name is Amit and I am learning AWS DevOps!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
